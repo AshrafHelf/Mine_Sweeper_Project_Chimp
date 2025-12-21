9c355f50740f0856596b358a4a7ae3ca1ae2e12c
Kareen333 <kareen.shamas@gmail.com>
2025-12-21 21:15:46 +0200

    Add JUnit tests

--- MineTry/src/junit/ToggleFlagRevealedCellTest.java
+++ MineTry/src/junit/ToggleFlagRevealedCellTest.java
@@ -1,6 +1,7 @@
 package junit;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import controller.GameEngine;
@@ -12,23 +13,57 @@ import model.Game;
 
 public class ToggleFlagRevealedCellTest {
 
-    @Test
-    public void toggleFlag_revealedCell_noChange() {
+    private GameEngine engine;
+    private Game game;
+    private Board board;
+    private Cell cell;
+
+    @Before
+    public void setUp() {
         BoardGenerator gen = new BoardGenerator();
-        GameEngine engine = new GameEngine(gen, null);
-        Game game = engine.newGame(Difficulty.EASY, "p1", "p2");
+        engine = new GameEngine(gen, null);
+        game = engine.newGame(Difficulty.EASY, "p1", "p2");
 
-        Board board = game.getBoardFor(game.getActivePlayer());
-        Cell cell = board.get(0, 0);
+        board = game.getBoardFor(game.getActivePlayer());
+        cell = board.get(0, 0);
 
+        // common precondition for all tests
         cell.setRevealed(true);
+    }
 
+    @Test
+    public void toggleFlag_revealedCell_flagStateUnchanged() {
         boolean flaggedBefore = cell.isFlagged();
-        int scoreBefore = game.getTeamScore();
 
         engine.toggleFlag(game, 0, 0);
 
         assertEquals(flaggedBefore, cell.isFlagged());
+    }
+
+    @Test
+    public void toggleFlag_revealedCell_scoreNotChanged() {
+        int scoreBefore = game.getTeamScore();
+
+        engine.toggleFlag(game, 0, 0);
+
         assertEquals(scoreBefore, game.getTeamScore());
+    }
+
+    @Test
+    public void toggleFlag_revealedCell_multipleToggles_noEffect() {
+        boolean flaggedBefore = cell.isFlagged();
+
+        engine.toggleFlag(game, 0, 0);
+        engine.toggleFlag(game, 0, 0);
+        engine.toggleFlag(game, 0, 0);
+
+        assertEquals(flaggedBefore, cell.isFlagged());
+    }
+
+    @Test
+    public void toggleFlag_revealedCell_remainsRevealed() {
+        engine.toggleFlag(game, 0, 0);
+
+        assertTrue(cell.isRevealed());
     }
 }
